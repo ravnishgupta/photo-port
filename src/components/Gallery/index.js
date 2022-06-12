000b3dd1181e0d5cd77ee7f4314486e78e2fa940
ravnishgupta <ravnish@gmail.com>
2022-06-12 00:13:30 -0700

    new photoList

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,26 +1,16 @@
 import React from "react";
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
+import PhotoList from "../PhotoList";
 
-function Gallery(props){
-    const currentCategory = {
-        name: "commercial",
-        description:
-          "Photos of grocery stores, food trucks, and other commercial projects",
-      };
+function Gallery(props) {
+    const { currentCategory } = props;
     return (
-        <section>
-          <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-          <p>{currentCategory.description}</p>
-          <div className="flex-row">
-            <img
-                src={photo}
-                alt="Commercial Example"
-                className="img-thumbnail mx-1"
-            />
-          </div>
-        </section>
-    )
-}
-
-export default Gallery;
+      <section>
+        <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+        <p>{currentCategory.description}</p>
+        <PhotoList category={currentCategory.name} />
+      </section>
+    );
+  }
+  export default Gallery;
+  
